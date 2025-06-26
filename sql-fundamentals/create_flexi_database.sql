a8c6d3cb26ce5f01a1e83ab8dab329e1d958a408
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-06-26 10:31:55 +0100

    Add SQL query to create flexi database and table structure

---
+++ sql-fundamentals/create_flexi_database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE flexi;
+
+USE flexi;
+
+CREATE TABLE flexi (
+    id INT PRIMARY KEY,
+    firstname VARCHAR(100),
+    lastname VARCHAR(100),
+    address TEXT,
+    occupation VARCHAR(100),
+    date DATE
+);
